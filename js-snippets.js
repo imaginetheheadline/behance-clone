1136c93ddd16843fc1c1b8fffe9007a0f814c4ac
imaginetheheadline <80075296+imaginetheheadline@users.noreply.github.com>
2022-04-01 20:13:39 +1100

    Add HTML and JS Snippets

---
+++ js-snippets.js
@@ -0,0 +1,30 @@
+// Find an element on the page with a CSS Selector
+const element = document.querySelector("<css selector>");
+
+// Find all elements on a page with matching CSS Selector
+const elements = document.querySelectorAll("<css selector>");
+
+// Do something when element is clicked
+element.addEventListener("click", () => {
+    // Do something here
+});
+
+// Add a CSS Class to an element
+element.classList.add("<class>");
+
+// Remove a CSS Class from an element
+modal.classList.remove("hidden");
+
+// Do something for each element in a set
+elements.forEach((element) => {
+    // Do something with element
+});
+
+// Get the value of the `src` attribute of an element, like an image
+let src = element.src;
+
+// Find and replace some text in a string
+string.replace("<search_text>", "<replace_text>");
+
+// Set the value of the `src` attribute of an element to something new
+modalImage.src = "<new thing>";
